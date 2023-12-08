78b02fb8f5a27805849f998613528f9bceb21395
ren1023 <renyou.1023@gmail.com>
2023-12-08 13:52:22 +0800

    建立新增資料api檔案add

---
+++ api/add.php
@@ -0,0 +1,29 @@
+<?php
+include_once "db.php";
+
+// $_POST
+
+
+// 有沒有上傳檔案
+// 把？？table存進去
+
+$DB=${ucfirst($_POST['table'])};
+$table=$_POST['table'];
+
+if(isset($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
+    $_POST['img']=$_FILES['img']['name'];
+
+}
+
+// 從post來有多了一個欄位！會與資料表對不上，故要將hidden的table的這個欄位刪除(unset)。
+unset($_POST['table']);
+$DB->save($_POST);
+
+// echo "test";
+// exit();
+
+
+to("../back.php?do=$table");
+
+?>
--- modal/title.php
+++ modal/title.php
@@ -2,7 +2,7 @@
     <!-- <?= $_GET['table']; ?> 可以用此方法確認是否有收到來源網頁傳來的值-->
 </h3>
 <hr>
-<form action="./api/add_title" method="post" enctype="multipart/form-data">
+<form action="./api/add.php" method="post" enctype="multipart/form-data">
     <table>
         <tr>
             <td>標題區圖片</td>
@@ -14,8 +14,12 @@
                 <input type="text" name="text" id="">
             </td>
         </tr>
+       
+    </table>
+
+    <div>
         <input type="hidden" name="table" value="<?= $_GET['table']; ?>">
         <input type="submit" value="新增">
         <input type="reset" value="重置">
-    </table>
+    </div>
 </form>
