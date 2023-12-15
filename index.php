f43371351cb38122f191958e381cbef0f05a3076
ren1023 <renyou.1023@gmail.com>
2023-12-15 16:32:13 +0800

    完成新聞更多消息顯示區

--- index.php
+++ index.php
@@ -51,7 +51,9 @@
 			}
 
 			?>
-			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
+
+			<!-- 以下這段程式碼會被移到front/news裡___start -->
+			<!-- <div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 			<script>
 				$(".sswww").hover(
 					function() {
@@ -66,7 +68,10 @@
 						$("#alt").hide()
 					}
 				)
-			</script>
+			</script> -->
+			<!-- 以下這段程式碼會被移到front/news裡___end -->
+
+
 			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
 				<!--右邊-->
 				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
