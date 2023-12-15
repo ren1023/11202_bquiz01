7c90dce91d1cdb3ecc08add6873e63e2db456cb4
ren1023 <renyou.1023@gmail.com>
2023-12-15 14:55:59 +0800

    完成顯示動畫圖片第一版

--- front/main_test.php
+++ front/main_test.php
@@ -1,33 +1,18 @@
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
-
     <?php include "marquee.php"; ?>
-
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
 
     <div style="width:100%; padding:2px; height:290px;">
         <div id="mwww" loop="true" style="width:100%; height:100%;">
-            <div style="width:99%; height:100%; position:relative;" class="cent">
-                沒有資料(放動畫圖片)
-                <!-- <img src="./img/01C01.gif" alt=""> -->
-            </div>
-            <!-- <embed loop=true src='"./img/01C01.gif" + lin[now] + "' style='width:99%; height:100%;'></embed> -->
-
+            <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
         </div>
     </div>
     <script>
-        // ##輪播照片的程式_start
         var lin = new Array();
-        // <?php
-            // $lins = $Mvim->all(['sh' => 1]); //撈出有被顯示的所有動畫圖片
-            // foreach ($lins as $lin) {
-            //     echo "lin.push('{$lin['img']}');";
-            // }
-            // 
-            ?>
-
         <?php
         $lins = $Mvim->all(['sh' => 1]);
+        print_r($lins);
         foreach ($lins as $lin) {
             echo "lin.push('{$lin['img']}');";
         }
@@ -36,20 +21,18 @@
         var now = 0;
         ww();
         if (lin.length > 1) {
-            setInterval("ww()", 3000); //讀到此行後，3秒後再執行，再每隔3秒，執行ww()方法一次。
+            setInterval("ww()", 3000);
             now = 1;
         }
 
-        function ww() { //用Jquery寫的function
-            $("#mwww").html("<embed loop=true src='./img/" + lin[now] + "' style='width:99%; height:100%;'></embed>") //要將一個html字串放到mwww裡
+        function ww() {
+            $("#mwww").html("<embed loop=true src='./img/" + lin[now] + "' style='width:99%; height:100%;'></embed>")
             //$("#mwww").attr("src",lin[now])
-            now++; // now這個值，+1之後
-            if (now >= lin.length) //now大於、等於 lin這個陣列的長度，now>=6 為true，就歸零
-                now = 0; //歸零
+            now++;
+            if (now >= lin.length)
+                now = 0;
         }
-        // ##輪播照片的程式_end
     </script>
-
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
         </span>
@@ -62,6 +45,7 @@
                 echo "...</li>";
             }
             ?>
+
         </ul>
         <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
         <script>
