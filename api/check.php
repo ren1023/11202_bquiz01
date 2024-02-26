b0d6fa07a656995efba82e9eecd242edb6ff28a3
ren1023 <renyou.1023@gmail.com>
2024-02-26 08:52:12 +0800

    加入備註

--- api/check.php
+++ api/check.php
@@ -2,7 +2,7 @@
 include_once "db.php";
 
 // " ' or 1=1; "=> SQL injection;
-
+// 解決 sql injection，將由表單接收來的資料，進行編碼後
 $acc=htmlspecialchars($_POST['acc']);
 $pw=htmlspecialchars($_POST['pw']);
 if($Admin->count(['acc'=>$acc,'pw'=>$pw])>0){
